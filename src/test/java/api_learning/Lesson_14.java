a784479abe8bf4164ed74927785910e6266e9389
Thu Pham <ptthu36@gmail.com>
2022-07-06 17:46:45 +0700

    Lesson 14 updated

--- src/test/java/api_learning/Lesson_14.java
+++ src/test/java/api_learning/Lesson_14.java
@@ -1,5 +1,45 @@
 package api_learning;
 
-public class Lesson_14 {
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Lesson_14 {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = null;
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("udid", "0A221FDD4000L2");
+        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+        try {
+            // {capabilities} ----> Appium Server:4723
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCapabilities);
+            Thread.sleep(3000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (appiumDriver != null) {
+            appiumDriver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
+
+            System.out.println("Connected to appium server and launched target app!");
+            MobileElement loginLabel = appiumDriver.findElementByAccessibilityId("Login");
+            loginLabel.click();
+
+        } else {
+            System.out.println("Error when connecting with Appium server!");
+        }
+    }
 }
+
+
