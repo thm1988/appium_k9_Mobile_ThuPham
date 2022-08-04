c0d20c61616f37476f48daabfc9c47d7d0e4b618
Thu Pham <ptthu36@gmail.com>
2022-08-04 12:48:08 +0700

    Lesson 20 final

---
+++ src/test/java/tests/testng/TestNGHooks02.java
@@ -0,0 +1,33 @@
+package tests.testng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGHooks02 extends BaseTestNG {
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("\t\t\t---> " + this.getClass().getSimpleName() + "|Before Class");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("\t\t\t\t---> " + this.getClass().getSimpleName() + "|Before Method");
+    }
+
+    @Test
+    public void testSth01() {
+        System.out.println(this.getClass().getSimpleName() + " Test method 01");
+    }
+
+    @Test
+    public void testSth02() {
+        System.out.println(this.getClass().getSimpleName() + " Test method 02");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("\t\t\t\t---> " + this.getClass().getSimpleName() + "|After Method");
+    }
+}
